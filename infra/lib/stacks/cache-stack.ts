d8e2d48058cc4c50cdc8d250726fea3eb5356020
haandol <ldg55d@gmail.com>
2022-10-24 11:47:57 +0900

    apply removalpolicy to subnetgroup

--- infra/lib/stacks/cache-stack.ts
+++ infra/lib/stacks/cache-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as elasticache from 'aws-cdk-lib/aws-elasticache';
@@ -42,6 +42,7 @@ export class CacheStack extends Stack {
         cacheSubnetGroupName: `${Config.Ns}SubnetGroup`,
       }
     );
+    subnetGroup.applyRemovalPolicy(RemovalPolicy.DESTROY);
 
     return new elasticache.CfnReplicationGroup(this, 'ReplicationGroup', {
       replicationGroupDescription: `${Config.Ns}Cluster`,
